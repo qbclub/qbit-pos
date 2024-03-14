339ed2f7da43149d9816033f3155050457f01525
qbclub <17926057+qbclub@users.noreply.github.com>
2024-03-14 08:12:52 +0400

    change on vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -16,17 +16,9 @@ import logo from '../assets/logo.svg'
         {{ key }}
     </div>
 
-    <UAccordion color="gray" variant="solid" size="xl"
-    class="container mx-auto"
-        :items="[{ label: '1. What is Nuxt UI?', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }, { label: '2. Getting Started', content: 'Lorem ipsum dolor sit amet, consectet' }, { label: '3. Theming', content: 'et, consectetur adipiscing elit' }, { label: '4. Components', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }]">
-      
-        <template #item="{ item }">
-            <p class="italic text-gray-900 dark:text-white ">
-                {{ item.content }}
-            </p>
-        </template>
-
-    </UAccordion>
+    <v-btn>
+        Button
+    </v-btn>
 
 
 
